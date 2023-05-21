39a9e231c5b416a9aa4a53659c8d5d88048b4408
Hernan Rinaldi <hernanjavierrinaldi@gmail.com>
2023-05-21 20:34:48 -0300

    feat/addNewWork

--- src/components/Portfolio/Data.jsx
+++ src/components/Portfolio/Data.jsx
@@ -15,16 +15,16 @@ export const projectsData = [
   {
     id: 3,
     image: Work3,
-    title: "Calculadora-ios",
-    category: "Movile",
-    demo:"https://www.linkedin.com/in/hernan-javier-rinaldi/overlay/experience/2116043291/multiple-media-viewer/?profileId=ACoAADVrpecBEGEr_lsO4s2MgqT_UiDHaUkrnJ0&treasuryMediaId=1635515861520"
+    title: "Track Travel",
+    category: "web",
+    demo:"https://track-travel-web.vercel.app"
   },
   {
     id: 2,
     image: Work2,
     title: "World Developers",
     category: "web",
-    demo:"https://world-dev-front.vercel.app/"
+    demo:"https://world-dev-front.vercel.app"
   },
   {
     id: 1,

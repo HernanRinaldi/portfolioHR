46f91f9e03545938f73199f5df1e9257e7aa62a2
Hernan Rinaldi <hernanjavierrinaldi@gmail.com>
2023-07-19 19:52:00 -0300

    feat/cleanCode

--- src/components/Portfolio/Data.jsx
+++ src/components/Portfolio/Data.jsx
@@ -3,37 +3,35 @@ import Work3 from "../../assets/Work3.png";
 import Work2 from "../../assets/Work2.png";
 import Work1 from "../../assets/Work1.png";
 
-
 export const projectsData = [
   {
     id: 4,
     image: Work4,
     title: "MoviesHome",
     category: "Movile",
-    demo:"https://github.com/HernanRinaldi/moviesHome/tree/main/src"
+    demo: "https://github.com/HernanRinaldi/moviesHome/tree/main/src",
   },
   {
     id: 3,
     image: Work3,
     title: "Track Travel",
     category: "web",
-    demo:"https://track-travel-web.vercel.app"
+    demo: "https://track-travel-web.vercel.app",
   },
   {
     id: 2,
     image: Work2,
     title: "World Developers",
     category: "web",
-    demo:"https://world-dev-front.vercel.app"
+    demo: "https://world-dev-front.vercel.app",
   },
   {
     id: 1,
     image: Work1,
     title: "Foods-App",
     category: "web",
-    demo:"https://hernan-app-foods.vercel.app"
-  }
-  
+    demo: "https://hernan-app-foods.vercel.app",
+  },
 ];
 
 // projects
@@ -46,6 +44,5 @@ export const projectsNav = [
   },
   {
     name: "Movile",
-  }
-
+  },
 ];
